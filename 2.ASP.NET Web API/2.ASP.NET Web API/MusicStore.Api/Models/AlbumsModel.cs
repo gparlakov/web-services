a16eac3064136e10876b0d6d5d30c42165950a4c
gparlakov <gparlakov@gmail.com>
2013-08-07 06:56:37 -0700

    WebAPI Add partial and full models to return

---
+++ 2.ASP.NET Web API/2.ASP.NET Web API/MusicStore.Api/Models/AlbumsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicStore.Api.Models
+{
+    public class AlbumsModel
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ 2.ASP.NET Web API/2.ASP.NET Web API/MusicStore.Api/Models/SongModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MusicStore.Api.Models
+{
+    public class SongModel
+    {
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+    }
+}
