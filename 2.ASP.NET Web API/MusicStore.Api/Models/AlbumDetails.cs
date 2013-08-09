619d53ff60e0bf89d3ae21b61070e5ed72093576
gparlakov <gparlakov@gmail.com>
2013-08-09 00:51:42 -0700

    WebAPI Album model and details added

---
+++ 2.ASP.NET Web API/MusicStore.Api/Models/AlbumDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Api.Models
+{
+    public class AlbumDetails : AlbumModel
+    {
+        public IEnumerable<ArtistModel> Artists { get; set; }
+
+        public IEnumerable<SongModel> Songs { get; set; }
+    }
+}
