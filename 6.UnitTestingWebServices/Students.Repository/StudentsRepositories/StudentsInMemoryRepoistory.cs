70d11122c74254be349035f32278a7253552b41e
gparlakov <gparlakov@gmail.com>
2013-08-16 10:07:01 -0700

    Repositories for students

---
+++ 6.UnitTestingWebServices/Students.Repository/StudentsRepositories/StudentsInMemoryRepoistory.cs
@@ -0,0 +1,74 @@
+﻿using Students.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Students.Repository
+{
+    public class StudentsInMemoryRepository : IRepository<Student>
+    {
+        private HashSet<Student> students;
+
+        public StudentsInMemoryRepository()
+        {
+            this.students = new HashSet<Student>();
+        }
+
+        public Student Add(Student element)
+        {
+            this.students.Add(element);
+            element.Id = students.Count;
+            return element;
+        }
+
+        public IQueryable<Student> All()
+        {
+            return this.students.AsQueryable();
+        }
+
+        public Student Get(int id)
+        {
+            var found = this.students.FirstOrDefault(s => s.Id == id);
+
+            return found;
+        }
+
+        public Student Edit(int id, Student element)
+        {
+            var found = this.Get(id);
+            if (found == null)
+            {
+                throw new InvalidOperationException(
+                    string.Format("No user with id {0} found", id));
+            }
+
+            this.students.Remove(found);
+            element.Id = id;
+            this.students.Add(element);            
+
+            return element;
+        }
+
+        public bool Delete(int id)
+        {
+            var deleted = false;
+
+            var found = this.Get(id);
+            if (found == null)
+            {
+                throw new InvalidOperationException(
+                    string.Format("No user with id {0} found", id));
+            }
+
+            return deleted;
+        }
+
+        public IQueryable<Student> Find(Expression<Func<Student, int, bool>> predicate)
+        {
+            var found = this.All().Where(predicate);
+
+            return found;
+        }
+    }
+}
---
+++ 6.UnitTestingWebServices/Students.Repository/StudentsRepositories/StudentsInSQLRepository.cs
@@ -0,0 +1,166 @@
+﻿using Students.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students.Repository
+{
+    public class StudentsInSQLRepository : IStudentsRepository
+    {
+        private const int MinNameLenght = 2;
+        private const int MaxNameLenght = 50;
+        private const int MaxGradeInSchool = 12;
+        private const int MinGradeInSchool = 1;
+
+        private DbStudentsContextFactory dbContextFactory;
+
+        public StudentsInSQLRepository(DbStudentsContextFactory dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+
+        public Student Add(Student element)
+        {
+            ValidateStudentModel(element);
+
+            var dbContext = this.dbContextFactory.Create();
+            
+            GetOrCreateSchool(element, dbContext);
+            
+            dbContext.Students.Add(element);
+            dbContext.SaveChanges();
+
+            return element;
+        }
+        
+        public IQueryable<Student> All()
+        {
+            var dbContext = this.dbContextFactory.Create();
+
+            return dbContext.Students;
+        }
+
+        public Student Get(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Id can not be non-positive");
+            }
+
+            var dbContext = this.dbContextFactory.Create();
+
+            return dbContext.Students.FirstOrDefault(s => s.Id == id);
+        }
+
+        private void GetOrCreateSchool(Student student, Data.StudentsDb dbContext)
+        {
+            var schoolInRepository = dbContext.Schools.FirstOrDefault(s => s.Name == student.School.Name);
+
+            if (schoolInRepository != null)
+            {
+                student.School = schoolInRepository;
+                return;
+            }
+
+            dbContext.Schools.Add(student.School);
+            dbContext.SaveChanges();
+        }
+
+        #region Not needed methods
+        // not needed
+        //public Models.Student Edit(int id, Models.Student element)
+        //{
+        //    var dbContext = this.dbContextFactory.Create();
+
+        //    dbContext.Students.Attach(element);
+        //    dbContext.SaveChanges();
+
+        //    return element;
+        //}
+
+        //public bool Delete(int id)
+        //{
+        //    var dbContext = this.dbContextFactory.Create();
+
+        //    var found = dbContext.Students.FirstOrDefault(s => s.Id == id);
+
+        //    var deleted = false;
+        //    if (found != null)
+        //    {
+        //        dbContext.Students.Remove(found);
+        //        deleted = true;
+        //    }
+
+        //    return deleted;
+        //}
+
+        //public IQueryable<Student> Find(Expression<Func<Student, int, bool>> predicate)
+        //{
+        //    var found = this.All().Where(predicate);
+
+        //    return found;
+        //} 
+        #endregion
+
+        #region Validate student model
+        private void ValidateStudentModel(Student element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException("Student can't be null!");
+            }
+
+            ValidateName(element);
+            ValidateSchool(element.School);
+            ValidateGrade(element.Grade);
+        }
+
+        private void ValidateGrade(int grade)
+        {
+            if (grade < MinGradeInSchool || grade > MaxGradeInSchool)
+            {
+                throw new ArgumentOutOfRangeException(
+                    string.Format("Students grade must be between {0} and {1}",
+                    MinGradeInSchool, MaxGradeInSchool));
+            }
+        }
+
+        private void ValidateSchool(School school)
+        {
+            if (school == null)
+            {
+                throw new ArgumentNullException("A student must have a school set!");
+            }
+            if (school.Name == null)
+            {
+                throw new ArgumentNullException("A student' school must have a name!");
+            }
+        }
+
+        private void ValidateName(Student element)
+        {
+            if (element.FirstName == null || element.LastName == null)
+            {
+                throw new ArgumentNullException("A student must have First and Last name");
+            }
+
+            if (element.FirstName.Length < MinNameLenght ||
+                element.LastName.Length < MinNameLenght)
+            {
+                throw new ArgumentOutOfRangeException(
+                    string.Format("Students names must be at least {0} symbols long", MinNameLenght));
+            }
+
+            if (element.FirstName.Length > MaxNameLenght ||
+                element.LastName.Length > MaxNameLenght)
+            {
+                throw new ArgumentOutOfRangeException(
+                    string.Format("Students names must be at most {0} symbols long", MaxNameLenght));
+            }
+        }
+        #endregion
+    }
+}
